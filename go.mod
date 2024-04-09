b9cff9c1195092edac691f3a9461933f000b8520
dlz <476439123@qq.com>
2024-04-09 11:43:55 +0800

    fix: cui

--- go.mod
+++ go.mod
@@ -2,15 +2,18 @@ module ssh-select
 
 go 1.21
 
-require github.com/rivo/tview v0.0.0-20240406141410-79d4cc321256
+require (
+	github.com/MichaelMure/go-term-text v0.3.1
+	github.com/awesome-gocui/gocui v1.1.0
+)
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.7.1 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	golang.org/x/sys v0.17.0 // indirect
-	golang.org/x/term v0.17.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- main.go
+++ main.go
@@ -6,21 +6,27 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"github.com/rivo/tview"
-	"github.com/gdamore/tcell/v2"
+	"github.com/awesome-gocui/gocui"
+	"log"
 	"os/exec"
 )
 
-// Config 表示配置信息的结构体
-type Config struct {
+var (
+	selectedServer *Server
+	serverIndex    = 0
+	servers        []Server
+)
+
+// Server 表示配置信息的结构体
+type Server struct {
 	Username string
 	IP       string
 	Port     string
-	Comment  string
+	Note     string
 }
 
 // parseLine 解析单行配置
-func parseLine(line string) (*Config, error) {
+func parseLine(line string) (*Server, error) {
 	// 分割用户名和后面的部分
 	parts := strings.Split(line, "@")
 	if len(parts) != 2 {
@@ -29,12 +35,12 @@ func parseLine(line string) (*Config, error) {
 	username := parts[0]
 
 	// 分割 IP:端口 和备注
-	ipPortComment := strings.Split(parts[1], "#")
-	if len(ipPortComment) < 2 {
+	ipPortNote := strings.Split(parts[1], "#")
+	if len(ipPortNote) < 2 {
 		return nil, fmt.Errorf("invalid format")
 	}
-	ipPort := ipPortComment[0]
-	comment := ipPortComment[1]
+	ipPort := ipPortNote[0]
+	note := ipPortNote[1]
 
 	// 分割 IP 和端口
 	ipPortParts := strings.Split(ipPort, ":")
@@ -44,23 +50,22 @@ func parseLine(line string) (*Config, error) {
 	ip := ipPortParts[0]
 	port := ipPortParts[1]
 
-	return &Config{
+	return &Server{
 		Username: username,
 		IP:       ip,
 		Port:     port,
-		Comment:  comment,
+		Note:     note,
 	}, nil
 }
 
-// readConfig 从文件读取配置
-func readConfig(filePath string) ([]Config, error) {
+// readServers 从文件读取配置
+func readServers(filePath string) error {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer file.Close()
 
-	var configs []Config
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -69,86 +74,205 @@ func readConfig(filePath string) ([]Config, error) {
 			fmt.Printf("Warning: Skipping line due to error: %s\n", err)
 			continue
 		}
-		configs = append(configs, *config)
+		servers = append(servers, *config)
 	}
 
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return err
 	}
 
-	return configs, nil
+	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	i := serverIndex
+
+	main, err := g.SetView("main", 0, 0, maxX-1, maxY-10, 0)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		main.Title = " Servers "
+		main.Wrap = true
+		main.Highlight = true
+		main.SelBgColor = gocui.ColorRed
+		main.SelFgColor = gocui.ColorWhite
+
+		if i == 0 {
+			for index, server := range servers {
+				if index+1 < 10 {
+					fmt.Fprintf(main, "  %d: %s \n", index+1, server.Note)
+				} else {
+					fmt.Fprintf(main, " %d: %s \n", index+1, server.Note)
+				}
+			}
+		}
+	}
+
+	detail, err := g.SetView("detail", 0, maxY-9, maxX-31, maxY-1, 0)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	detail.Clear()
+	detail.Title = " Connection Details "
+	fmt.Fprintln(detail, " ")
+	fmt.Fprintf(detail, " IP       : %s\n", servers[i].IP)
+	fmt.Fprintf(detail, " Username : %s\n", servers[i].Username)
+	fmt.Fprintf(detail, " Port     : %s\n", servers[i].Port)
+
+	help, err := g.SetView("help", maxX-30, maxY-9, maxX-1, maxY-1, 0)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+
+	help.Clear()
+	help.Title = " Keybindings "
+	fmt.Fprintln(help, " ")
+	fmt.Fprintln(help, "    ↑ ↓: Select")
+	fmt.Fprintln(help, "     ↵ : Connect")
+	fmt.Fprintln(help, "     ^C: Exit")
+
+	if _, err := g.SetCurrentView("main"); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
 	// 使用 flag 包来解析命令行参数
-	configFilePath := flag.String("config", "hosts.txt", "Path to the configuration file")
+	configFilePath := flag.String("c", "servers.txt", "Path to the configuration file")
 	flag.Parse()
 
-	configs, err := readConfig(*configFilePath)
+	err := readServers(*configFilePath)
 	if err != nil {
 		fmt.Println("Error reading config:", err)
 		return
 	}
 
-	// 初始化 tview 应用
-	app := tview.NewApplication()
+	selectServer()
 
-	// 创建一个列表组件
-	list := tview.NewList()
-	list.ShowSecondaryText(false)
+	connectServer()
+}
 
-	var host Config
-
-	// 添加配置到列表中
-	for i, config := range configs {
-		config := config // 捕获循环变量
-		list.AddItem(fmt.Sprintf("%d.%s(%s@%s:%s)", i+1, config.Comment, config.Username, config.IP, config.Port), "", 0, func() {
-			host = config
-			app.Stop()
-		})
+func connectServer() {
+	if selectedServer == nil {
+		return
 	}
 
-	// 设置搜索功能
-	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyRune:
-			if event.Rune() == '/' {
-				// 触发搜索模式，这里仅仅是一个示例
-				// 实际中，你可能需要实现一个更复杂的搜索逻辑
-				app.Suspend(func() {
-					fmt.Print("Enter search keyword: ")
-					var keyword string
-					fmt.Scanln(&keyword)
-					list.Clear()
-					for _, config := range configs {
-						if strings.Contains(config.Username, keyword) || strings.Contains(config.IP, keyword) || strings.Contains(config.Comment, keyword) {
-							config := config // 捕获循环变量
-							list.AddItem(fmt.Sprintf("%s@%s:%s", config.Username, config.IP, config.Port), config.Comment, 0, func() {
-								// 处理选择事件
-								fmt.Printf("Selected config: %s@%s:%s\n", config.Username, config.IP, config.Port)
-								app.Stop()
-							})
-						}
-					}
-				})
-				return nil // 处理按键事件，不进一步传播
-			}
-		}
-
-		// 对于未处理的按键，允许默认操作
-		return event
-	})
-
-	if err := app.SetRoot(list, true).Run(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println(host)
-
-	cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", host.Username, host.IP), "-p", host.Port)
+	cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", selectedServer.Username, selectedServer.IP), "-p", selectedServer.Port)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	_ = cmd.Run()
+}
+
+func selectServer() {
+	g, err := gocui.NewGui(gocui.OutputNormal, false)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := initKeybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func initKeybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, selected); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("main", gocui.KeyArrowUp, gocui.ModNone, scrollUp); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("main", gocui.KeyArrowDown, gocui.ModNone, scrollDown); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func selected(g *gocui.Gui, v *gocui.View) error {
+	selectedServer = &servers[serverIndex]
+	return gocui.ErrQuit
+}
+
+func scrollUp(g *gocui.Gui, v *gocui.View) error {
+	if serverIndex == 0 {
+		serverIndex = len(servers) - 1
+	} else {
+		serverIndex -= 1
+	}
+
+	_, sY := v.Size()
+
+	// 选择最后一行时：视图的窗口移动到末尾
+	if serverIndex == len(servers)-1 {
+		if len(servers) > sY {
+			v.SetOrigin(0, len(servers)-sY+1)
+			v.SetCursor(0, sY-1)
+		} else {
+			v.SetCursor(0, len(servers)-1)
+		}
+		return nil
+	}
+
+	_, oy := v.Origin()
+	if oy == 0 {
+		// 首行出现：移动光标，不移动视图的窗口
+		v.MoveCursor(0, -1)
+	} else {
+		// 首行未出现：移动视图的窗口，不移动光标
+		v.SetOrigin(0, oy-1)
+	}
+
+	return nil
+}
+
+func scrollDown(g *gocui.Gui, v *gocui.View) error {
+	if serverIndex >= len(servers)-1 {
+		serverIndex = 0
+	} else {
+		serverIndex += 1
+	}
+
+	// 选择第一行时，视图的窗口移动到顶部
+	if serverIndex == 0 {
+		v.SetOrigin(0, 0)
+		v.SetCursor(0, 0)
+		return nil
+	}
+
+	_, sY := v.Size()
+	if serverIndex >= sY {
+		// 超出可视区（即前 N 行）：移动视图的窗口，不移动光标
+		if err := v.SetOrigin(0, serverIndex-sY+1); err != nil {
+			return err
+		}
+	} else if serverIndex < sY {
+		// 可视区（即前 N 行）内：移动光标，不移动视图的窗口
+		v.MoveCursor(0, 1)
+	}
+
+	return nil
 }
